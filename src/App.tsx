d7946a05871287cb1ef59a5af80838f62ca475d1
Ronaldo Ortiz <ronyortizop@gmail.com>
2023-09-26 16:32:28 -0400

    Initial commit - shadcn, tailwindcss configured

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-red-700">Hello there</h2>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
